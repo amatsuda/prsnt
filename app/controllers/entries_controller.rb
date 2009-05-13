2afc036d8f4eea9e808c4052069fbbdc658199fe
Akira Matsuda <ronnie@dio.jp>
2009-05-13 22:11:09 +0900

    implement entry CRUD

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,85 +1,61 @@
 class EntriesController < ApplicationController
   # GET /entries
-  # GET /entries.xml
   def index
+    @event = Event.find params[:event_id]
     @entries = Entry.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @entries }
-    end
   end
 
   # GET /entries/1
-  # GET /entries/1.xml
   def show
+    @event = Event.find params[:event_id]
     @entry = Entry.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @entry }
-    end
   end
 
   # GET /entries/new
-  # GET /entries/new.xml
   def new
+    @event = Event.find params[:event_id]
     @entry = Entry.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @entry }
-    end
   end
 
   # GET /entries/1/edit
   def edit
+    @event = Event.find params[:event_id]
     @entry = Entry.find(params[:id])
   end
 
   # POST /entries
-  # POST /entries.xml
   def create
+    @event = Event.find params[:event_id]
     @entry = Entry.new(params[:entry])
+    @entry.event = @event
 
-    respond_to do |format|
-      if @entry.save
-        flash[:notice] = 'Entry was successfully created.'
-        format.html { redirect_to(@entry) }
-        format.xml  { render :xml => @entry, :status => :created, :location => @entry }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @entry.errors, :status => :unprocessable_entity }
-      end
+    if @entry.save
+      flash[:notice] = 'Entry was successfully created.'
+      redirect_to event_entries_path(@event)
+    else
+      render :action => "new"
     end
   end
 
   # PUT /entries/1
-  # PUT /entries/1.xml
   def update
     @entry = Entry.find(params[:id])
 
     respond_to do |format|
       if @entry.update_attributes(params[:entry])
         flash[:notice] = 'Entry was successfully updated.'
-        format.html { redirect_to(@entry) }
-        format.xml  { head :ok }
+        redirect_to(@entry)
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @entry.errors, :status => :unprocessable_entity }
+        render :action => "edit"
       end
     end
   end
 
   # DELETE /entries/1
-  # DELETE /entries/1.xml
   def destroy
     @entry = Entry.find(params[:id])
     @entry.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(entries_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(entries_url)
   end
 end
